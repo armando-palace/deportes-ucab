7dd71def03270dd38a3718f52b812e198d1a17d5
Jesús Di Bari <iijbari@gmail.com>
2018-09-20 02:48:57 -0400

    Created homepage and header (navbar)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'welcome#index'
+
   devise_for :users
 
   namespace :admin do
-    get "landing" => "landing#index"
+    get 'landing' => 'landing#index'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
