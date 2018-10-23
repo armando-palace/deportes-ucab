284ca3abf17486182727c701a5e1886b17a87ab9
Jesús Di Bari <iijbari@gmail.com>
2018-10-22 22:31:00 -0400

    Users can be edited

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,10 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :get_roles, only: [:new, :edit]
 
   def new
     @user = User.new
-    @roles = Role.all
   end
 
   def create
@@ -12,13 +13,23 @@ class Admin::UsersController < ApplicationController
     if @user.save
       redirect_to [:admin, @user]
     else
-      @roles = Role.all
+      # @roles = Role.all
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to [:admin, @user]
+    else
+      render :edit
+    end
+  end
+
   def show
-    @user = User.find(params[:id])
   end
 
   def index
@@ -26,6 +37,14 @@ class Admin::UsersController < ApplicationController
   end
 
   private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def get_roles
+      @roles = Role.all
+    end
 
     def user_params
       params.require(:user).permit(
